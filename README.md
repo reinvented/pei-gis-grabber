445caf6a97bc8c3524e9c1249e4b2061e56bc998
Peter Rukavina <peter@rukavina.net>
2019-05-05 20:04:50 -0300

    Updated for new download mechanism and added QGIS Python script generation.

--- README.md
+++ README.md
@@ -1,31 +1,40 @@
 PEI GIS Grabber
 ===============
 
-The Province of Prince Edward Island, Canada provides access to [free GIS data via its website](http://www.gov.pe.ca/gis/index.php3?number=77543&lang=E).
+The Province of Prince Edward Island, Canada provides access to [GIS data via its website](http://www.gov.pe.ca/gis/index.php3?number=77543&lang=E).
 
-Unfortunately they have an (inane) requirement to enter name, email and occupation for every download, making complete download of all files cumbersome.
-
-This script solves that problem by automating the download.
+This script allows you to easily download *all* of the GIS data available in a form suitable for loading into [QGIS](https://www.qgis.org/) for analysis and manipulation.
 
 Requirements
 ------------
 
 * PHP 5.x
-* PHP Simple HTML DOM Parser (from http://simplehtmldom.sourceforge.net/)
-* wget (from http://www.gnu.org/s/wget/)
+* [PHP Simple HTML DOM Parser](http://simplehtmldom.sourceforge.net/)
+* [wget](http://www.gnu.org/s/wget/)
 
 Usage
 -----
 
-Edit the file scrape-gis.php and check and/or modify the $downloadDir and $wget variables.
+Edit the file scrape-gis.php and check and/or modify the **$downloadDir**, **$wget** and **$zip** variables as required.
 
-You may also need to modify the $gisPages array if the "Free GIS Products" links on the [main GIS catalog page](http://www.gov.pe.ca/gis/index.php3?number=77543&lang=E) change or are expanded.
+You may also need to modify the *$gisPages* array if the "Free GIS Products" links on the [main GIS catalog page](http://www.gov.pe.ca/gis/index.php3?number=77543&lang=E) change or are expanded.
 
-Once everything is in place just do a:
+Once everything is in place:
 
     php scrape-gis.php
 
-and the all of the SHP files linked to from the various free GIS data pages will be downloaded. You can modify the script if you want to get only MID/MIF or NTX files, or only files of a certain name.
+and the all of the SHP files linked to from the various free GIS data pages will be downloaded, aggregated into folders based on the page they were scraped from. You can modify the script if you want to get only MID/MIF or NTX files, or only files of a certain name.
+
+Import into QGIS
+----------------
+
+Once all the data has been scraped, you will end up with a Python script called *importlayers.py* in your **$downloadDir**.
+
+From within QGIS, open the Python Console (**Plugins > Python Console**), click on the *Show Editor* icon on the Python Console toolbar, and then the *Open Script* icon in the Editor, and open *importlayers.py* and click the *Run* icon.
+
+The script will execute on paste, and the end result should be a QGIS project containing all of the PEI GIS layers:
+
+![QGIS Screen Shot](images/qgis-screen-shot.png)
 
 Caution
 -------
@@ -37,4 +46,4 @@ This code is not guaranteed to work properly, or at all, and I have no connectio
 Credits
 -------
 
-[Peter Rukavina, Reinvented Inc.](http://ruk.ca/)
+[Peter Rukavina](https://ruk.ca/)
--- scrape-gis.php
+++ scrape-gis.php
@@ -9,7 +9,7 @@
   * for every download, making complete download of all files cumbersome.
   *
   * This script solves that problem by automating the download.
-  *     
+  *
   * This program is free software; you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation; either version 2 of the License, or (at
@@ -25,27 +25,39 @@
   * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
   * USA
   *
-  * @version 0.1, October 12, 2011
+  * @version 0.2, May 5, 2019
   * @author Peter Rukavina <peter@rukavina.net>
-  * @copyright Copyright &copy; 2011, Reinvented Inc.
+  * @copyright Copyright &copy; 2019, Reinvented Inc.
   * @license http://www.fsf.org/licensing/licenses/gpl.txt GNU Public License
   */
 
 /**
-  * This is an array of HTML pages on www.gov.pe.ca that hold links to GIS files. Add or modify the links as required.
-  */
-$gisPages = array("page1.html" => "http://www.gov.pe.ca/gis/index.php3?number=77868&lang=E",
-                  "page2.html" => "http://www.gov.pe.ca/gis/index.php3?number=1012857&lang=E");
-
-/**
   * Where to put the GIS files we download. Include trailing slash.
   */
-$downloadDir = "gisdata/";
+$downloadDir = "/Volumes/Working/Code/pei-gis/gisdata/";
 
 /**
   * Where is the wget utility?
   */
-$wget = "/sw/bin/wget";
+$wget = "/usr/local/bin/wget";
+
+/**
+  * Where is the unzip utility?
+  */
+$unzip = "/usr/bin/unzip";
+
+/**
+  * This is an array of HTML pages on www.gov.pe.ca that hold links to GIS files. Add or modify the links as required.
+  */
+$gisPages = array("basemap"         => "http://www.gov.pe.ca/gis/index.php3?number=77581&lang=E",
+                  "community"       => "http://www.gov.pe.ca/gis/index.php3?number=77584&lang=E",
+                  "emergency"       => "http://www.gov.pe.ca/gis/index.php3?number=77552&lang=E",
+                  "index"           => "http://www.gov.pe.ca/gis/index.php3?number=77551&lang=E",
+                  "resource"        => "http://www.gov.pe.ca/gis/index.php3?number=77555&lang=E",
+                  "civicaddress"    => "http://www.gov.pe.ca/gis/index.php3?number=77553&lang=E",
+                  "electoral"       => "http://www.gov.pe.ca/gis/index.php3?number=77554&lang=E",
+                  "imagery"         => "http://www.gov.pe.ca/gis/index.php3?number=77582&lang=E",
+                  "transportation"  => "http://www.gov.pe.ca/gis/index.php3?number=77583&lang=E");
 
 /**
   * Get this from http://simplehtmldom.sourceforge.net/
@@ -57,21 +69,53 @@ require_once("simple_html_dom.php");
   * "license_agreement.php3", which indicates they're a link to a map file download.
   */
 $maplinks = array();
-foreach($gisPages as $filename => $url) {
-    $dom = file_get_html($url);
-    $links = $dom->find('td a[href^=license_agreement.php3]');
-    foreach ($links as $l) {
-        array_push($maplinks,$l->attr['href']);
-    }
+foreach($gisPages as $folder => $url) {
+  $dom = file_get_html($url);
+  $links = $dom->find('td a[href^=license_agreement.php3]');
+  foreach ($links as $l) {
+      array_push($maplinks,array("folder" => $folder, "link" => $l->attr['href']));
+  }
 }
 
 /**
   * Now use 'wget' to download every SHP file to directly.
   */
-foreach ($maplinks as $key => $link) {
-    parse_str($link,$linkparts);
-    if ($linkparts['amp;file_format'] == "SHP") {
-        $fileURL = "http://www.gov.pe.ca/photos/original/" . $linkparts['license_agreement_php3?name'] . ".SHP.zip";
-        system("$wget -O \"$downloadDir" . $linkparts['license_agreement_php3?name'] . ".SHP.zip\" $fileURL");
+
+$fp = fopen($downloadDir . "importlayers.py", "w");
+fwrite($fp, "import os\n");
+fwrite($fp, "from qgis.core import (\n");
+fwrite($fp, "\tQgsVectorLayer\n");
+fwrite($fp, ")\n");
+fwrite($fp, "root = QgsProject.instance().layerTreeRoot()\n");
+
+$created_group = array();
+foreach ($maplinks as $key => $maplink) {
+  parse_str($maplink['link'], $linkparts);
+  if ($linkparts['amp;file_format'] == "SHP") {
+    $inputfile = "http://www.gov.pe.ca/photos/original/" . $linkparts['license_agreement_php3?name'] . ".SHP.zip";
+    if (!file_exists($downloadDir . $maplink['folder'])) {
+      mkdir($downloadDir . $maplink['folder']);
     }
+    $outputfile = $downloadDir . $maplink['folder'] . '/' . $linkparts['license_agreement_php3?name'] . ".SHP.zip";
+    $outputdirectory = $downloadDir . $maplink['folder'] . '/' . $linkparts['license_agreement_php3?name'] . ".SHP";
+    if (!file_exists($outputfile)) {
+      system("$wget -O \"$outputfile\" $inputfile");
+      system("$unzip -d $outputdirectory $outputfile");
+    }
+    foreach (glob("$outputdirectory/*.shp") as $filename) {
+      if (!array_key_exists($maplink['folder'], $created_group)) {
+        fwrite($fp, "mygroup = root.addGroup(\"" . $maplink['folder'] . "\")\n");
+        $created_group[$maplink['folder']] = TRUE;
+      }
+      else {
+        fwrite($fp, "mygroup = root.findGroup(\"" . $maplink['folder'] . "\")\n");
+      }
+      fwrite($fp, "mylayer = QgsVectorLayer(\"" . $filename . "\", \"" . $linkparts['license_agreement_php3?name'] . "\", \"ogr\")\n");
+      fwrite($fp, "QgsProject.instance().addMapLayer(mylayer, False)\n");
+      fwrite($fp, "mygroup.addLayer(mylayer)\n");
+      fwrite($fp, "QgsProject.instance().layerTreeRoot().findLayer(mylayer.id()).setItemVisibilityChecked(False)\n");
+    }
+  }
 }
+
+fclose($fp);
